dfb5d2815ece81459dd37cddd8bfad1b074877ea
Dell <dnirmalasari18@gmail.com>
2017-11-17 02:05:43 +0700

    hehein aja

--- soal3.c
+++ soal3.c
@@ -1,98 +1,352 @@
-#define FUSE_USE_VERSION 28
+
 #include <fuse.h>
 #include <stdio.h>
-#include<stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
-#include <sys/time.h>
+#include <sys/statfs.h>
+//thanks to blog fandyuhuy
+static const char *dirpath = "/home/dell/Unduhan"; //Destinasi folder yang akan di mountkan
 
-static const char *dirpath = "/home/dell/Documents";
+char globalpath[1000];
+int bendera=0;
 
-static int xmp_getattr(const char *path, struct stat *stbuf)
+static int hm_getattr(const char *path, struct stat *stbuf)
 {
-  int res;
-	char fpath[1000];
-	sprintf(fpath,"%s%s",dirpath,path);
-	res = lstat(fpath, stbuf);
+    printf("xmp_getattr: %s\n", path);
+    int res;
 
-	if (res == -1)
-		return -errno;
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = lstat(fpath, stbuf);
+    if(res == -1)
+        return -errno;
 
-	return 0;
+    return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
+static int hm_readlink(const char *path, char *buf, size_t size)
 {
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
-	}
-	else sprintf(fpath, "%s%s",dirpath,path);
-	int res = 0;
+    printf("xmp_readlink: %s\n", path);
+    int res;
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = readlink(fpath, buf, size - 1);
+    if(res == -1)
+        return -errno;
 
-	DIR *dp;
-	struct dirent *de;
-
-	(void) offset;
-	(void) fi;
-
-	dp = opendir(fpath);
-	if (dp == NULL)
-		return -errno;
-
-	while ((de = readdir(dp)) != NULL) {
-        struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		res = (filler(buf, de->d_name, &st, 0));
-			if(res!=0) break;
-	}
-    system("notify-send hello");
-	closedir(dp);
-	return 0;
+    buf[res] = '\0';
+    return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
+
+static int hm_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler)
 {
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
-	}
-	else sprintf(fpath, "%s%s",dirpath,path);
-	int res = 0;
-  int fd = 0 ;
+    printf("xmp_getdir: %s\n", path);
+    DIR *dp;
+    struct dirent *de;
+    int res = 0;
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
 
-	(void) fi;
-	fd = open(fpath, O_RDONLY);
-	if (fd == -1)
-		return -errno;
+    dp = opendir(fpath);
+    if(dp == NULL)
+        return -errno;
 
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
+    while((de = readdir(dp)) != NULL) {
+        res = filler(h, de->d_name, de->d_type);
+        if(res != 0)
+            break;
+    }
 
-	close(fd);
-	return res;
+    closedir(dp);
+    return res;
 }
 
-static struct fuse_operations xmp_oper = {
-	.getattr	= xmp_getattr,
-	.readdir	= xmp_readdir,
-	.read		= xmp_read,
+static int hm_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+    printf("xmp_mknod: %s\n", path);
+    int res;
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+
+    res = mknod(fpath, mode, rdev);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_mkdir(const char *path, mode_t mode)
+{
+    printf("xmp_mkdir: %s\n", path);
+    int res;
+
+char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = mkdir(fpath, mode);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_unlink(const char *path)
+{
+    printf("xmp_unlink: %s\n", path);
+    int res;
+char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = unlink(fpath);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_rmdir(const char *path)
+{
+    printf("xmp_rmdir: %s\n", path);
+    int res;
+
+char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = rmdir(fpath);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_symlink(const char *from, const char *to)
+{
+    printf("xmp_symlink: %s\n", path);
+    int res;
+char fpath [1000], ffrom[1000],fto[1000];
+    sprintf(fto,"%s%s",dirpath,to);
+    sprintf(ffrom,"%s%s",dirpath,from);
+   
+   
+    res = symlink(ffrom, fto);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_rename(const char *from, const char *to)
+{
+    printf("xmp_rename: %s\n", path);
+    int res;
+
+    char fpath [1000], ffrom[1000],fto[1000];
+    sprintf(fto,"%s%s",dirpath,to);
+    sprintf(ffrom,"%s%s",dirpath,from);
+    res = rename(ffrom, fto);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_link(const char *from, const char *to)
+{
+    printf("xmp_link: %s\n", path);
+    int res;
+
+    char fpath [1000], ffrom[1000],fto[1000];
+    sprintf(fto,"%s%s",dirpath,to);
+    sprintf(ffrom,"%s%s",dirpath,from);
+    res = link(ffrom, fto);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_chmod(const char *path, mode_t mode)
+{
+    printf("xmp_chmod: %s\n", path);
+    int res;
+
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = chmod(fpath, mode);
+    if(res == -1)
+        return -errno;
+   
+    return 0;
+}
+
+static int hm_chown(const char *path, uid_t uid, gid_t gid)
+{
+    printf("xmp_chown: %s\n", path);
+    int res;
+
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = lchown(fpath, uid, gid);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_truncate(const char *path, off_t size)
+{
+    printf("xmp_truncate: %s\n", path);
+    int res;
+
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);   
+    res = truncate(fpath, size);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int hm_utime(const char *path, struct utimbuf *buf)
+{
+    printf("xmp_utime: %s\n", path);
+    int res;
+   
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = utime(fpath, buf);
+    if(res == -1)
+        return -errno;
+
+    return 0;
+}
+
+
+static int hm_open(const char *path, int flags)
+{
+    printf("xmp_open: %s\n", path);
+    int res;
+
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    res = open(fpath, flags);
+    if(res == -1)
+        return -errno;
+
+    close(res);
+    return 0;
+}
+
+static int hm_read(const char *path, char *buf, size_t size, off_t offset)
+{
+    printf("xmp_read: %s\n", path);
+    int fd;
+    int res;
+
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    fd = open(fpath, O_RDONLY);
+    if(fd == -1)
+        return -errno;
+
+    strcpy(globalpath,fpath);
+
+    res = pread(fd, buf, size, offset);
+    if(res == -1)
+        res = -errno;
+   
+    close(fd);
+    return res;
+}
+
+static int hm_write(const char *path, const char *buf, size_t size,
+                     off_t offset)
+{
+    printf("xmp_write: %s\n", path);
+    int fd;
+    int res;
+
+        chmod(globalpath,0000);
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    fd = open(fpath, O_WRONLY);
+    if(fd == -1)
+        return -errno;
+
+    res = pwrite(fd, buf, size, offset);
+    if(res == -1)
+        res = -errno;
+   
+    close(fd);
+    return res;
+}
+
+/*static int xmp_statfs(struct fuse_statfs *fst)
+{
+    struct statfs st;
+    int rv = statfs("/",&st);
+    if(!rv) {
+        fst->block_size  = st.f_bsize;
+        fst->blocks      = st.f_blocks;
+        fst->blocks_free = st.f_bavail;
+        fst->files       = st.f_files;
+        fst->files_free  = st.f_ffree;
+        fst->namelen     = st.f_namelen;
+    }
+    return rv;
+}
+*/
+static int hm_release(const char *path, int flags)
+{
+    printf("xmp_release: %s\n", path);
+    /* Just a stub.  This method is optional and can safely be left
+       unimplemented */
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    (void) fpath;
+    (void) flags;
+    return 0;
+}
+
+static int hm_fsync(const char *path, int isdatasync)
+{
+    printf("xmp_fsync: %s\n", path);
+    /* Just a stub.  This method is optional and can safely be left
+       unimplemented */
+    char fpath [1000];
+    sprintf(fpath,"%s%s",dirpath,path);
+    (void) fpath;
+    (void) isdatasync;
+    return 0;
+}
+
+static struct fuse_operations hm_oper = {
+    .getattr    = hm_getattr,
+    .readlink    = hm_readlink,
+    .getdir    = hm_getdir,
+    .mknod    = hm_mknod,
+    .mkdir    = hm_mkdir,
+    .symlink    = hm_symlink,
+    .unlink    = hm_unlink,
+    .rmdir    = hm_rmdir,
+    .rename    = hm_rename,
+    .link    = hm_link,
+    .chmod    = hm_chmod,
+    .chown    = hm_chown,
+    .truncate    = hm_truncate,
+    .utime    = hm_utime,
+    .open    = hm_open,
+    .read    = hm_read,
+    .write    = hm_write,
+   // .statfs    = xmp_statfs,
+    .release    = hm_release,
+    .fsync    = hm_fsync
+   
 };
 
 int main(int argc, char *argv[])
 {
-	umask(0);
-	return fuse_main(argc, argv, &xmp_oper, NULL);
-}
+    fuse_main(argc, argv, &hm_oper);
+    return 0;
+} 
