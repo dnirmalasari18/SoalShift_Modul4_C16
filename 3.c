1174845b8ccbe37e84fe9856bf0a75499b0fd2f6
Dell <dnirmalasari18@gmail.com>
2017-11-16 18:09:08 +0700

    nambah nambah git

--- 3.c
+++ 3.c
@@ -1,20 +1,22 @@
 #define FUSE_USE_VERSION 28
 #include <fuse.h>
 #include <stdio.h>
-#include<stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
-
-static const char *dirpath = "/home/dell/Documents";
+#include <stdlib.h>
+static const char *dirpath = "/home/dell/Unduhan";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
-  int res;
+	printf("xmp_getattr: %s\n", path);
+	int res;
 	char fpath[1000];
+ //   char newFile[1000];
+  //  memcpy(newFile, path, strlen(path));
 	sprintf(fpath,"%s%s",dirpath,path);
 	res = lstat(fpath, stbuf);
 
@@ -27,7 +29,8 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		       off_t offset, struct fuse_file_info *fi)
 {
-  char fpath[1000];
+	printf("xmp_readdir: %s\n", path);
+	char fpath[1000];
 	if(strcmp(path,"/") == 0)
 	{
 		path=dirpath;
@@ -47,14 +50,14 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		return -errno;
 
 	while ((de = readdir(dp)) != NULL) {
-        struct stat st;
+		struct stat st;
 		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
 		st.st_mode = de->d_type << 12;
 		res = (filler(buf, de->d_name, &st, 0));
 			if(res!=0) break;
 	}
-    system("notify-send hello");
+
 	closedir(dp);
 	return 0;
 }
@@ -62,37 +65,112 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
 {
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
-	}
-	else sprintf(fpath, "%s%s",dirpath,path);
+	printf("xmp_read: %s\n", path);
+	char fpath[1000];
+    char newFile[1000];
+    sprintf(fpath,"%s",path);
 	int res = 0;
-  int fd = 0 ;
+	int fd = 0 ;
+    if(strcmp(path,"/") == 0)
+	{
+		memcpy(newFile, path, strlen(path));
+		// path=dirpath;
+		sprintf(fpath,"%s",newFile);
+	}
+	else {
+		memcpy(newFile, path, strlen(path));
+		//newFile[strlen(path)] = '\0';
+		sprintf(fpath, "%s%s",dirpath,newFile);
+	}
+	
+    (void) fi;
+    fd = open(fpath, O_RDONLY);
+    if (fd == -1)
+        return -errno;
 
-	(void) fi;
-	fd = open(fpath, O_RDONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
+    res = pread(fd, buf, size, offset);
+    if (res == -1)
+        res = -errno;
+    
+    close(fd);
+    return res;
 }
 
+static int xmp_write(const char *path, const char *buf, size_t size,
+                     off_t offset, struct fuse_file_info *fi)
+{
+    printf("xmp_write: %s\n", path);
+        int fd;
+        int res;
+        (void) fi;
+        if(fi == NULL)
+                fd = open(path, O_WRONLY);
+        else
+                fd = fi->fh;
+        
+        if (fd == -1)
+                return -errno;
+        res = pwrite(fd, buf, size, offset);
+        if (res == -1)
+                res = -errno;
+        if(fi == NULL)
+                close(fd);
+        return res;
+}
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+    printf("xmp_open: %s\n", path);
+        int res;
+        res = open(path, fi->flags);
+        if (res == -1)
+                return -errno;
+        fi->fh = res;
+        return 0;
+}
+
+static int xmp_release(const char *path, struct fuse_file_info *fi)
+{
+    printf("xmp_release: %s\n", path);
+        (void) path;
+        close(fi->fh);
+        return 0;
+}
+
+static int xmp_create(const char *path, mode_t mode,
+                      struct fuse_file_info *fi)
+{
+    printf("xmp_create: %s\n", path);
+        int res;
+        res = open(path, fi->flags, mode);
+        if (res == -1)
+                return -errno;
+        fi->fh = res;
+        return 0;
+}
+
+static int xmp_access(const char *path, int mask)
+{
+    printf("xmp_access: %s\n", path);
+        int res;
+        res = access(path, mask);
+        if (res == -1)
+                return -errno;
+        return 0;
+}
 static struct fuse_operations xmp_oper = {
 	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
 	.read		= xmp_read,
+    .write		= xmp_write,
+    .open		= xmp_open,
+    .release = xmp_release,
+    .create = xmp_create,
+    .access = xmp_access,
 };
 
 int main(int argc, char *argv[])
 {
-	umask(0);
+	//umask(0);
 	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
